f1783c342d2ee902f3107e4af9fe7a98ae30afcd
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-15 17:19:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,16 +21,26 @@ def monopoly_with_third_tier
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
+<<<<<<< HEAD
       :four_pieces_owned=>200
+=======
+      :four_pieces_owned=>200,
+>>>>>>> 3b7b0cee443aadfa3dbb96d5038e55dce255b39e
     },
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
+<<<<<<< HEAD
         }
       }
     }
+=======
+      }
+    }
+
+>>>>>>> 3b7b0cee443aadfa3dbb96d5038e55dce255b39e
 end
 
 def monopoly_with_fourth_tier
